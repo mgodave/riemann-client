44a960bcfdb0ab8b70647e83076e65de570f447d
Dave Rusek <dave.rusek@gmail.com>
2012-10-23 22:00:01 -0600

    Clients now uses daemon threads

--- client/src/main/java/org/robobninjas/riemann/Clients.java
+++ client/src/main/java/org/robobninjas/riemann/Clients.java
@@ -18,22 +18,34 @@
 
 package org.robobninjas.riemann;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.jboss.netty.channel.socket.oio.OioClientSocketChannelFactory;
 import org.jboss.netty.channel.socket.oio.OioDatagramChannelFactory;
 
 import javax.annotation.concurrent.ThreadSafe;
 
+import java.util.concurrent.ExecutorService;
+
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.concurrent.Executors.newCachedThreadPool;
 import static org.robobninjas.riemann.Client.DEFAULT_PORT;
 
 @ThreadSafe
 public class Clients {
 
+  private static ExecutorService getExecutorService() {
+    return newCachedThreadPool(
+      new ThreadFactoryBuilder()
+        .setDaemon(true)
+        .setNameFormat("Riemann Client Thread %1")
+        .build());
+  }
+
   public static UdpClient makeUdpClient(String address, int port) {
     checkNotNull(address, "Address cannot be null");
     checkArgument((port > 0) && (port < 65535), "Port number must be between 0 and 65535");
-    final OioDatagramChannelFactory channelFactory = new OioDatagramChannelFactory();
+    final OioDatagramChannelFactory channelFactory = new OioDatagramChannelFactory(getExecutorService());
     return new UdpClient(channelFactory, address, port);
   }
 
@@ -42,14 +54,13 @@ public class Clients {
   }
 
   public static UdpClient makeUdpClient() {
-    final OioDatagramChannelFactory channelFactory = new OioDatagramChannelFactory();
-    return new UdpClient(channelFactory);
+    return makeUdpClient("localhost");
   }
 
   public static TcpClient makeTcpClient(String address, int port) {
     checkNotNull(address, "Address cannot be null");
     checkArgument((port > 0) && (port < 65535), "Port number must be between 0 and 65535");
-    final OioClientSocketChannelFactory channelFactory = new OioClientSocketChannelFactory();
+    final OioClientSocketChannelFactory channelFactory = new OioClientSocketChannelFactory(getExecutorService());
     return new TcpClient(channelFactory, address, port);
   }
 
@@ -58,8 +69,7 @@ public class Clients {
   }
 
   public static TcpClient makeTcpClient() {
-    final OioClientSocketChannelFactory channelFactory = new OioClientSocketChannelFactory();
-    return new TcpClient(channelFactory);
+    return makeTcpClient("localhost");
   }
 
 }
