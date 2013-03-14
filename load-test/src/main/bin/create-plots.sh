6a9ec397291692a53e485a867baea6cf76dfac6e
Dave Rusek <dave.rusek@gmail.com>
2013-03-14 11:14:13 -0600

    Added a plots.html output for quick viewing of the generated plots

--- load-test/src/main/bin/create-plots.sh
+++ load-test/src/main/bin/create-plots.sh
@@ -10,4 +10,17 @@ PREFIX="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	gnuplot < $PREFIX/throughput.plot
 	gnuplot < $PREFIX/unacked.plot
 
+	cat <<EOF > plots.html
+
+<html>
+	<body>
+		<img src="acks.png"/><p/>
+		<img src="rtt.png"/><p/>
+		<img src="throughput.png"/><p/>
+		<img src="unacked.png"/><p/>
+	</body>
+</html>
+
+EOF
+
 )
